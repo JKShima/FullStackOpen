ea68011a9d2795bbf39b2c47df4965f9a06cbc70
Kenshin Shimabukuro <jkshimabukuro@pucp.edu.pe>
2023-10-17 16:49:49 -0500

    Exercise 1.8

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -8,15 +8,17 @@ const Button = ({ handleClick, text}) => (
 
 const Statistics = (props) => {
   //console.log('Statistics ', props)
+  const total = props.good + props.neutral + props.bad
+
   return (
     <div>
       <h1>Statistics</h1>
       <p>good {props.good}</p>
       <p>neutral {props.neutral}</p>
       <p>bad {props.bad}</p>
-      <p>all {props.total}</p>
-      <p>average {(props.good*1 + props.neutral*0 + props.bad*(-1))/props.total}</p>
-      <p>positive {(props.good/props.total)*100} %</p>
+      <p>all {total}</p>
+      <p>average {(props.good*1 + props.neutral*0 + props.bad*(-1))/total}</p>
+      <p>positive {(props.good/total)*100} %</p>
     </div>
   )
   
@@ -28,25 +30,19 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  // Exercise 1.7
-  const [total, setTotal] = useState(0)
-
   const handleGoodClick = () => {
     const updatedGood = good + 1
     setGood(updatedGood)
-    setTotal(updatedGood + neutral + bad)
   }
 
   const handleNeutralClick = () => {
     const updatedNeutral = neutral + 1
     setNeutral(updatedNeutral)
-    setTotal(updatedNeutral + good + bad)
   }
 
   const handleBadClick = () => {
     const updatedBad = bad + 1
     setBad(updatedBad)
-    setTotal(updatedBad + good + neutral)
   }
 
   return (
@@ -55,7 +51,7 @@ const App = () => {
       <Button handleClick={handleGoodClick} text='good'/>
       <Button handleClick={handleNeutralClick} text='neutral'/>
       <Button handleClick={handleBadClick} text='bad'/>
-      <Statistics good={good} neutral={neutral} bad={bad} total={total}/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
