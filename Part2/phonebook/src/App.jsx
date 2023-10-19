194f13732aac78bbb92cd4247ec180d810f4c8bb
Kenshin Shimabukuro <jkshimabukuro@pucp.edu.pe>
2023-10-19 16:33:15 -0500

    Exercises 2.9 and 2.10

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import PersonsList from './components/PersonsList'
 import PersonsForm from './components/PersonsForm'
+import Filter from './components/Filter'
 
 const App = () => {
-  const [persons, setPersons] = useState([]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]) 
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
+  const [filterName, setFilterName] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -37,9 +45,20 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    const query = event.target.value
+    setFilterName(query)
+  }
+
+  const filteredNames = persons.map(props => props.name.toLowerCase().includes(filterName.toLowerCase()))
+  ? persons.filter(props => props.name.toLowerCase().includes(filterName.toLowerCase()))
+  : persons
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <Filter filter ={filterName} onChange={handleFilterChange}/>
+      <h2>Add a new</h2>
       <PersonsForm 
         addPerson={addPerson}
         newName={newName}
@@ -49,7 +68,7 @@ const App = () => {
       />
       <h2>Numbers</h2>
       <div>
-        <PersonsList persons={persons}/>
+        <PersonsList persons={filteredNames}/>
       </div>
     </div>
   )
