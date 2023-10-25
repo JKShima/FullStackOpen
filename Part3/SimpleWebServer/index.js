939f07d4296cbefa2a69f0f8e6b8b55d4ab9d4c1
Kenshin Shimabukuro <jkshimabukuro@pucp.edu.pe>
2023-10-25 14:24:07 -0500

    Fetching and deleting, postman

--- Part3/SimpleWebServer/index.js
+++ Part3/SimpleWebServer/index.js
@@ -22,6 +22,10 @@ let notes = [
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!!!</h1>')
 })
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+  })
   
 app.get('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
@@ -32,6 +36,13 @@ app.get('/api/notes/:id', (request, response) => {
     } else {
         response.status(404).end()
     }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+  
+    response.status(204).end()
 })
 
 const PORT = 3001
