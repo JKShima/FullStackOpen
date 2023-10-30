da29dc9a68c34cf18697ed40c3aaf0ba91b4c411
Kenshin Shimabukuro <jkshimabukuro@pucp.edu.pe>
2023-10-30 17:46:27 -0500

    MongoDB creation

--- Part4/index.js
+++ Part4/index.js
@@ -12,7 +12,9 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+const password = process.argv[2]
+
+const mongoUrl = `mongodb+srv://JKShima:${password}@cluster0.hghmr3y.mongodb.net/blogListApp?retryWrites=true&w=majority`
 mongoose.connect(mongoUrl)
 
 app.use(cors())
