a70cab017cacb814afbfc6d3d82e2c6b4af5722b
Kenshin Shimabukuro <jkshimabukuro@pucp.edu.pe>
2023-10-31 15:04:58 -0500

    Exercise 4.5

--- Part4/utils/list_helper.js
+++ Part4/utils/list_helper.js
@@ -9,8 +9,26 @@ const totalLikes = (blogs) => {
         return blogs.reduce((sum, blog) => sum + blog.likes,0)
     }
 }
+
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0){
+        return null
+    } else {
+        const mostLikedBlog = blogs.reduce(
+            (prev, current) => {
+                return prev.likes > current.likes ? prev : current
+            }
+        )
+        return ({
+            title: mostLikedBlog.title,
+            author: mostLikedBlog.author,
+            likes: mostLikedBlog.likes
+        })
+    }
+}
   
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
