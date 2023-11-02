1274ee1357f74fd4d73060812746dea69d3f9009
Kenshin Shimabukuro <jkshimabukuro@pucp.edu.pe>
2023-11-02 16:01:06 -0500

    Exercise 4.16

--- Part4/controllers/users.js
+++ Part4/controllers/users.js
@@ -5,18 +5,24 @@ const User = require('../models/user')
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
+  if (password === undefined || username === undefined){
+    return response.status(401).json({ error: 'username and password are required'})
+  } else if (password.length < 3 || username.length < 3){
+    return response.status(401).json({ error: 'username and password must be at least 3 characters long'})
+  } else {
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
 
-  const user = new User({
-    username,
-    name,
-    passwordHash,
-  })
+    const user = new User({
+        username,
+        name,
+        passwordHash,
+    })
 
-  const savedUser = await user.save()
+    const savedUser = await user.save()
 
-  response.status(201).json(savedUser)
+    response.status(201).json(savedUser)
+  }
 })
 
 usersRouter.get('/', async (request, response) => {
