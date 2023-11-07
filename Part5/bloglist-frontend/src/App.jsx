c78eeac36c9b7ba0dd6270205e008d7784aeda58
Kenshin Shimabukuro <jkshimabukuro@pucp.edu.pe>
2023-11-07 18:04:07 -0500

    Exercise 5.10

--- Part5/bloglist-frontend/src/App.jsx
+++ Part5/bloglist-frontend/src/App.jsx
@@ -130,9 +130,11 @@ const App = () => {
         <Togglable buttonLabel="New Blog" ref={blogFormRef}>
           <BlogForm createBlog={addBlog}/>
         </Togglable>
-        {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} updateLikes={updateLikes}/>
-        )}
+        {blogs
+          .sort((a, b) => b.likes - a.likes)
+          .map(blog =>
+            <Blog key={blog.id} blog={blog} updateLikes={updateLikes}/>
+          )}
       </div>
     )
   }
