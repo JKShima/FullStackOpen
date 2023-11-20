3daaf36d6fb97025dcf775f08d84adfdb77570bd
Kenshin Shimabukuro <jkshimabukuro@pucp.edu.pe>
2023-11-20 16:15:00 -0500

    Exercise 6.9

--- Part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ Part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -3,7 +3,16 @@ import { voteAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
   const dispatch = useDispatch()
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => {
+    if (state.filter === null) {
+      return state.anecdotes.sort((a,b) => b.votes - a.votes)
+    }
+    return state.anecdotes.filter((anecdote) => 
+      anecdote.content.toLowerCase()
+      .includes(state.filter.toLowerCase()))
+      .sort((a, b) => b.votes - a.votes)
+  })
+
 
   const vote = (id) => {
     dispatch(voteAnecdote(id))
@@ -12,7 +21,6 @@ const AnecdoteList = () => {
   return (
     <div>
       {anecdotes
-        .sort((a, b) => b.votes - a.votes)
         .map(anecdote =>
           <div key={anecdote.id}>
             <div>
